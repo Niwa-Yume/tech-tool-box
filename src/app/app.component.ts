3dfc4c8244db8800e22bca87d3b0f76b7009dffd
Julien Castro <julien.castro@protonmail.com>
2024-08-10 15:26:23 +0200

    ajout du composant a propos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeroSectionComponent, HeaderComponent],  // Importez votre composant autonome ici
+  imports: [HeroSectionComponent, HeaderComponent, AboutComponent],  // Importez votre composant autonome ici
   standalone: true,  // Si AppComponent est autonome
 })
 export class AppComponent {
