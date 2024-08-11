6cf33b58abe6fdea0c0debcd87ed84b8a091a904
Julien Castro <julien.castro@protonmail.com>
2024-08-11 18:43:08 +0200

    Fix des liens du footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+    socialLinks = {
+      x: "https://x.com/Niwa76156428",  // Remplacez par votre lien X
+      instagram: "https://www.instagram.com/niwa_yume",  // Remplacez par votre lien Instagram
+      portfolio: "https://portfolio-design-figma-alpha.vercel.app/",  // Remplacez par votre lien de portfolio
+      linkedin: "https://www.linkedin.com/in/julien-castro/",  // Remplacez par votre lien LinkedIn
+    };
+
+    openLink(url: string): void {
+      window.open(url, '_blank');
+    }
+
 }
+
