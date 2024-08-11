f97f57062438872aad80e10aa0c1c527c8ab6e63
Julien Castro <julien.castro@protonmail.com>
2024-08-11 15:55:06 +0200

    footer + cta liste ressource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from "./about/about.component";
 import { FeaturesComponent } from "./features/features.component";
+import { CtaComponent } from "./cta/cta.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeroSectionComponent, HeaderComponent, AboutComponent, FeaturesComponent],  // Importez votre composant autonome ici
+  imports: [HeroSectionComponent, HeaderComponent, AboutComponent, FeaturesComponent, CtaComponent, FooterComponent],  // Importez votre composant autonome ici
   standalone: true,  // Si AppComponent est autonome
 })
 export class AppComponent {
