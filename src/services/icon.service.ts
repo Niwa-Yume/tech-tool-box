4983f53e5ed13980a461f5665a36bba7a2dd1469
Julien Castro <julien.castro@protonmail.com>
2024-10-05 21:32:50 +0200

    ajout des ... pour description

--- src/services/icon.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-@Injectable({
-  providedIn: 'root'
-})
-export class IconService {
-}
